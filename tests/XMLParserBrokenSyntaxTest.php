b560c80fff5dbec9da8b1d95b1048efa43c26bb9
macbre <maciej.brencz@gmail.com>
2023-08-08 11:18:19 +0100

    Introduce XMLParserTestCase::streamFromString helper and remove then broken.xml file fixture

--- tests/XMLParserBrokenSyntaxTest.php
+++ tests/XMLParserBrokenSyntaxTest.php
@@ -4,8 +4,10 @@ use Elecena\XmlIterator\Exceptions\ParsingError;
 
 class XMLParserBrokenSyntaxTest extends XMLParserTestCase
 {
+	const BROKEN_XML = '<?xml version="1.0" encoding="utf-8" ?><foo></bar>';
+
 	protected function getParserStream() {
-		return fopen( __DIR__ . '/fixtures/broken.xml', mode: 'rt');
+		return self::streamFromString(self::BROKEN_XML);
 	}
 
 	public function testShouldThrowAnException(): void
--- tests/XMLParserTestCase.php
+++ tests/XMLParserTestCase.php
@@ -13,4 +13,17 @@ abstract class XMLParserTestCase extends TestCase
 	protected function getParser(): XMLParser {
 		return new XMLParser(stream: $this->getParserStream());
 	}
+
+	/**
+	 * Returns a readable, in-memory stream with the provided string as a content
+	 *
+	 * @return resource
+	 */
+	protected static function streamFromString(string $string) {
+		$stream = fopen('php://memory','r+');
+		fwrite($stream, $string);
+		rewind($stream);
+
+		return $stream;
+	}
 }
